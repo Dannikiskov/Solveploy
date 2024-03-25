07167e8c8fa166c4b0eeb8ce88aa45b97c705925
Dannikiskov <114243245+Dannikiskov@users.noreply.github.com>
2024-03-25 14:56:45 +0100

    Update prereqs.sh

--- scripts/prereqs.sh
+++ scripts/prereqs.sh
@@ -2,6 +2,6 @@ sudo apt install pipx -y
 pipx install --include-deps ansible
 ansible-galaxy collection install community.docker
 ansible-galaxy collection install community.kubernetes
-ansible-galaxy role install geerlingguy.docker
-ansible-galaxy role install gantsign.minikube
-ansible-galaxy role install robertdebock.kubectl
+ansible-galaxy role install --roles-path=ansible/playbooks/roles geerlingguy.docker
+ansible-galaxy role install --roles-path=ansible/playbooks/roles gantsign.minikube
+ansible-galaxy role install --roles-path=ansible/playbooks/roles robertdebock.kubectl
