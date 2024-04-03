c4b67946e7f01f2856aa4306be787ffe03818e17
Dannikiskov <114243245+Dannikiskov@users.noreply.github.com>
2024-04-03 13:15:26 +0200

    Update prereqs.sh

--- scripts/prereqs.sh
+++ scripts/prereqs.sh
@@ -4,12 +4,11 @@ export PATH=$PATH:/home/${USER}/.local/bin
 pip3 install openshift
 pip3 install pyyaml
 pip3 install kubernetes
-sudo usermod -aG docker $USER && newgrp docker
-sudo systemctl start docker
-ansible-galaxy collection install kubernetes.core
+ansible-galaxy collection install kubernetes.core --force
 ansible-galaxy role install --roles-path=ansible/playbooks/roles geerlingguy.docker
 ansible-galaxy role install --roles-path=ansible/playbooks/roles gantsign.minikube
 ansible-galaxy role install --roles-path=ansible/playbooks/roles robertdebock.kubectl
-
 ansible-playbook ansible/playbooks/install_roles.yml -i ansible/inventory/hosts
+sudo usermod -aG docker $USER && newgrp docker
+sudo systemctl start docker
 ansible-playbook ansible/playbooks/testbook.yml -i ansible/inventory/hosts
