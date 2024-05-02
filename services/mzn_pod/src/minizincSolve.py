85cee5b609a65836062b6d89a0c97004af497c3b
Danni Kiskov <danni@kiskov.me>
2024-05-02 06:11:52 +0200

    WIP

--- services/mzn_pod/src/minizincSolve.py
+++ services/mzn_pod/src/minizincSolve.py
@@ -50,5 +50,6 @@ def run_minizinc_model(model_string, solver_name, data_string=None, data_type=No
         execution_time = result.statistics['time'].total_seconds() * 1000
     else:
         execution_time = "N/A"
+        
     print({"result": output, "executionTime": execution_time, "status": str(result.status), "optValue": opt_val}, flush=True)
     return {"result": output, "executionTime": execution_time, "status": str(result.status), "optValue": opt_val}
