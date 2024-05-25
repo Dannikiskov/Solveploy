fb8e79535ca8a24e3bf78a6f15a2be610c630218
Danni Kiskov <danni@kiskov.me>
2024-05-26 01:57:06 +0200

    frontend test

--- services/frontend/src/Info.tsx
+++ services/frontend/src/Info.tsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 // import { useEffect } from "react";
 
+import { Key, useState } from "react";
 import "./App.css";
 
 
@@ -8,13 +9,16 @@ import "./App.css";
 
 
 function Info() {
+    const [data, setData] = useState([]);
 
-    const funct = () => {
-      fetch("/api/data")
+    const funct = async () => {
+      const response = await fetch("/api/data")
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
+          return data; // Return the data instead of void
         });
+      setData(response);
     };
 
 
@@ -22,8 +26,23 @@ function Info() {
     <>
         <h1>Information</h1>
         <p>
-            <button onClick={funct}> Click me </button>
+            <button onClick={funct}> Refresh data</button>
         </p>
+        <ul>
+          {(data as any[]).map((item, index) => (
+            <li key={index}>
+              {Array.isArray(item) ? (
+                <ul>
+                  {item.map((subItem: any, subIndex: Key | null | undefined) => (
+                    <li key={subIndex}>{JSON.stringify(subItem)}</li>
+                  ))}
+                </ul>
+              ) : (
+                JSON.stringify(item)
+              )}
+            </li>
+          ))}
+        </ul>
     </>
   );
 }
