4ce03be3c2dad0309e688b78bf0dd34fd13304c9
Danni Kiskov <danni@kiskov.me>
2024-05-26 02:03:21 +0200

    frontend test

--- services/frontend/src/Info.tsx
+++ services/frontend/src/Info.tsx
@@ -1,54 +1,58 @@
-// import { useState } from "react";
-// import { useEffect } from "react";
-
-import { Key, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
-
-
-
+interface DataItem {
+  status: string;
+  opt_goal: string;
+  opt_value: string;
+  data_file_name: string;
+  execution_time: string;
+}
 
 function Info() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    const funct = async () => {
-      const response = await fetch("/api/data")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          return data; // Return the data instead of void
-        });
-      setData(response);
-    };
-
+  const funct = async () => {
+    const response = await fetch("/api/data")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      });
+    setData(response);
+  };
 
   return (
     <>
-        <h1>Information</h1>
-        <p>
-            <button onClick={funct}> Refresh data</button>
-        </p>
-        <ul>
-          {(data as any[]).map((item, index) => (
-            <li key={index}>
-              {Array.isArray(item) ? (
+      <h1>Information</h1>
+      <p>
+        <button onClick={funct}> Refresh data</button>
+      </p>
+      <ul>
+        {(data as any[]).map((item, index) => (
+          <li key={index}>
+            <h2>{item[0]}</h2> {/* This is the solver name */}
+            {Object.entries(item[1]).map(([fileName, fileData], fileIndex) => (
+              <div key={fileIndex}>
+                <h3>{fileName}</h3> {/* This is the file name */}
                 <ul>
-                  {item.map((subItem: any, subIndex: Key | null | undefined) => (
-                    <li key={subIndex}>{JSON.stringify(subItem)}</li>
+                  {(fileData as DataItem[]).map((dataItem, dataIndex) => (
+                    <li key={dataIndex}>
+                      Status: {dataItem.status} <br />
+                      Opt Goal: {dataItem.opt_goal} <br />
+                      Opt Value: {dataItem.opt_value} <br />
+                      Data File Name: {dataItem.data_file_name} <br />
+                      Execution Time: {dataItem.execution_time}
+                    </li>
                   ))}
                 </ul>
-              ) : (
-                JSON.stringify(item)
-              )}
-            </li>
-          ))}
-        </ul>
+              </div>
+            ))}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default Info;
-
-
-
-
