95e668f574ee62853bd2f138f3a971a763e62d07
Danni Kiskov <danni@kiskov.me>
2024-05-29 06:40:06 +0200

    sat data get

--- services/job_handler/src/mznHandler.py
+++ services/job_handler/src/mznHandler.py
@@ -44,7 +44,8 @@ def handle_new_mzn_job(data):
     feature_vector = feature_vector.split(',')
     feature_vector = [str(float(i)) if 'e' in i else i for i in feature_vector]
     feature_vector = ','.join(feature_vector)
-    # print("\n\n\nFeature vector: ", feature_vector, "\n\n\n", flush=True)
+
+    print(f"{solver_name}: {k8s_result['status']}: {data['satFileName']}", flush=True)
     
     if k8s_result["status"] not in ["ERROR", "FAILED"]:
         dict = {"dataFileName": data["dataFileName"],
