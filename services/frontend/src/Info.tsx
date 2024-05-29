6ba7b2c41a3af5205178bf44be55f195fab784a1
Danni Kiskov <danni@kiskov.me>
2024-05-29 03:24:02 +0200

    mzn pod

--- services/frontend/src/Info.tsx
+++ services/frontend/src/Info.tsx
@@ -10,26 +10,49 @@ interface DataItem {
 }
 
 function Info() {
-  const [data, setData] = useState([]);
+  const [mznData, setMznData] = useState([]);
+  const [satData, setSatData] = useState([]);
+  const [maxsatData, setMaxsatData] = useState([]);
 
-  const funct = async () => {
-    const response = await fetch("/api/data")
+  const satGet = async () => {
+    const response = await fetch("/api/data/sat")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         return data;
       });
-    setData(response);
+      setSatData(response);
+  }
+
+  const maxsatGet = async () => {
+    const response = await fetch("/api/data/maxsat")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      });
+      setMaxsatData(response);
+  }
+
+  const mznGet = async () => {
+    const response = await fetch("/api/data/mzn")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      });
+      setMznData(response);
   };
 
   return (
     <>
       <h1>Information</h1>
+      <h2>Minizinc</h2>
       <p>
-        <button onClick={funct}> Refresh data</button>
+        <button onClick={mznGet}> Refresh Minizinc data</button>
       </p>
       <ul>
-        {data.map((solverData, index) => (
+        {mznData.map((solverData, index) => (
           <ul key={index}>
             <h2 style={{ border: '1px solid black', display: 'inline-block', padding: '8px', color: 'white' }}>{solverData[0]}</h2>
             {Object.entries(solverData[1] as Record<string, unknown>).map(([fileName, fileData], fileIndex) => (
@@ -59,6 +82,22 @@ function Info() {
           </ul>
         ))}
         <hr style={{margin: '2px'}} />
+      </ul>
+      <br />
+      <h2>SAT</h2>
+      <p>
+        <button onClick={satGet}> Refresh SAT data</button>
+        <ul>
+          {satData}
+        </ul>
+      </p>
+      <br />
+      <h2>MAXSAT</h2>
+      <p>
+        <button onClick={maxsatGet}> Refresh MAXSAT data</button>
+      </p>
+      <ul>
+        {maxsatData}
       </ul>
     </>
   );
