2bc334251aa4ed2992c42bb66e418933bd705aca
Danni Kiskov <danni@kiskov.me>
2024-05-30 02:27:43 +0200

    db

--- services/job_handler/src/sunny.py
+++ services/job_handler/src/sunny.py
@@ -100,7 +100,7 @@ def get_nearest_neighbors(feat_vect, k, solver_type):
 
 
 def get_sub_portfolio(similar_insts, solvers, T, solver_type):
-
+    solvers = [solver['name'] for solver in solvers]
 
     # Generate all possible subsets of solvers
     subsets = []
