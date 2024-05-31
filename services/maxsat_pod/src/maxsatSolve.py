3f2c7cc8ae56b6a922299f63d70a7c433ee456a7
Danni Kiskov <danni@kiskov.me>
2024-06-01 01:42:36 +0200

    maxsat update

--- services/maxsat_pod/src/maxsatSolve.py
+++ services/maxsat_pod/src/maxsatSolve.py
@@ -13,24 +13,27 @@ def run_maxsat_model(solver_name, cnf_string):
     weighted_cnf = False
     # Add clauses to the solver
     print("Adding clauses...\n", flush=True)
-    for line in cnf_lines:
-        if line.startswith('c'):
-            # Skip comments and problem line
-            pass
-        elif line.startswith('p'):
-            if "wcnf" in line:
-                weighted_cnf = True
-        else:
-            if weighted_cnf:
-                clause = [int(x) for x in line.split(" ")[:-1]]
-                weight = int(clause[0])
-                weightless_clause = clause[1:]
-                print("CLAUSE: ", clause, "WEIGHT: ", weight, "WEIGHTLESS_CLAUSE: ", weightless_clause, flush=True)
-                w_formula.append(weightless_clause, weight=weight)
+    try:
+        for line in cnf_lines:
+            if line.startswith('c'):
+                # Skip comments and problem line
+                pass
+            elif line.startswith('p'):
+                if "wcnf" in line:
+                    weighted_cnf = True
             else:
-                clause = [int(x) for x in line.split(" ")[:-1]]
-                print("CLAUSE: ", clause, "WEIGHT: ", weight, "WEIGHTLESS_CLAUSE: ", weightless_clause, flush=True)
-                formula.append(weightless_clause)
+                if weighted_cnf:
+                    clause = [int(x) for x in line.split(" ")[:-1]]
+                    weight = int(clause[0])
+                    weightless_clause = clause[1:]
+                    print("CLAUSE: ", clause, "WEIGHT: ", weight, "WEIGHTLESS_CLAUSE: ", weightless_clause, flush=True)
+                    w_formula.append(weightless_clause, weight=weight)
+                else:
+                    clause = [int(x) for x in line.split(" ")[:-1]]
+                    print("CLAUSE: ", clause, "WEIGHT: ", weight, "WEIGHTLESS_CLAUSE: ", weightless_clause, flush=True)
+                    formula.append(weightless_clause)
+    except Exception as e:
+        print("Error: ", e, flush=True)
 
     model = None
     t1 = None
