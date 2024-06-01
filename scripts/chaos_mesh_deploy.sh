dc3b63f162c33be1f8352004103c7260301db101
Dannikiskov <114243245+Dannikiskov@users.noreply.github.com>
2024-06-01 10:21:56 +0200

    Delete scripts directory

--- scripts/chaos_mesh_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-helm repo add chaos-mesh https://charts.chaos-mesh.org
-kubectl create ns chaos-mesh
-helm install chaos-mesh chaos-mesh/chaos-mesh -n=chaos-mesh --version 2.6.3
--- scripts/rmq_dashboard.sh
+++
@@ -1,4 +0,0 @@
-password="$(kubectl get secret rabbitmq -o jsonpath='{.data.rabbitmq-password}' | base64 --decode)"
-echo "password: $password"
-
-kubectl port-forward svc/rabbitmq -n  8080:4369
